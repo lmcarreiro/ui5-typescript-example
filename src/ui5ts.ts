83a9d43ee842475b332d4ec66d704a47cad04b48
Leonardo Machado Carreiro <leonardo.mcarreiro@gmail.com>
2017-10-04 15:57:14 -0300

    Component.ts changed from ui5 module/class definition to a typescript/es6 module/class definition. Some import errors, because imported files extensions are still *.js and not *.ts

---
+++ src/ui5ts.ts
@@ -0,0 +1,31 @@
+function UI5(name: string): any {
+    return function (target: FunctionConstructor, propertyKey: string, descriptor: PropertyDescriptor) {
+        var functionMembers: string[] = Object.getOwnPropertyNames(function () {});
+        var staticMembers:   string[] = Object.getOwnPropertyNames(target).filter(o => functionMembers.indexOf(o) === -1);
+        var instanceMethods: string[] = Object.getOwnPropertyNames(target.prototype);
+        
+        var baseClass: any = Object.getPrototypeOf(target); // é o mesmo que: baseClass = target.__proto__;
+
+        var thisClass: any = {};
+        staticMembers  .forEach(m => thisClass[m] = (<any>target)[m]);
+        instanceMethods.forEach(m => thisClass[m] = (<any>target.prototype)[m]);
+
+        if (typeof baseClass.extend === "function") {
+            return baseClass.extend(name, thisClass);
+        }
+        else {
+            throw new Error("A classe base não é uma classe UI5.");
+        }
+    }
+}
+
+function define(aDependencies: string[], vFactory: (...args: any[]) => any): any
+{
+    //remove as depenências "require" e "exports" geradas pelo typescript
+    var newDependencies = aDependencies.slice(2);
+
+    //passa null no lugar das dependências "require" e "exports" geradas pelo typescript e substitui coloca as dependências fornecidas pelo ui5 como default
+    var newFactory = (...args: any[]) => vFactory(null, null, ...args.map((d: any) => ({ default: d })));
+
+    return sap.ui.define(newDependencies, newFactory);
+}
